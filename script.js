78c4a2e0fee7829fb9a9d12968ec7ab84d322d5a
CaptZyy <148044525+CaptZyy@users.noreply.github.com>
2023-11-23 16:49:59 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+let a = 6.2;
+let b = 4.6;
+let c = 6.4;
+
+//math round
+let bulatkan = Math.round (a);
+console.log(bulatkan);
+
+let bulatkan2 = Math.round (b);
+console.log(bulatkan2);
+
+let bulatkan3 = Math.round (c);
+console.log(bulatkan3);
+
+//math floor
+let bawah = Math.floor(a);
+console.log(bawah);
+
+let bawah2 = Math.floor(b);
+console.log(bawah2);
+
+let bawah3 = Math.floor(c);
+console.log(bawah3);
+
+//math ceil
+let atas = Math.ceil(a);
+console.log(atas);
+
+let atas2 = Math.ceil(b);
+console.log(atas2);
+
+let atas3 = Math.ceil(c);
+console.log(atas3);
+
+//math pow
+
+let pangkat = Math.pow(3, 3);
+console.log(pangkat);
+
+let pangkat2 = Math.pow(5, 3);
+console.log(pangkat2);
+
+let pangkat3 = Math.pow(4, 3);
+console.log(pangkat3);
+
+//math sqrt
+
+let akar = Math.sqrt(25)
+console.log(akar);
+
+let akar2 = Math.sqrt(225)
+console.log(akar2);
+
+let akar3 = Math.sqrt(64)
+console.log(akar3);
